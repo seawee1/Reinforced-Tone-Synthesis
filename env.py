d2ab8b191db7b5e7dbce384473f1e8a0fc6d49b3
Sebastian Rietsch <sebrie75@gmail.com>
2020-06-15 17:02:48 +0200

    Progress

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import gym
 from gym import spaces
-from gym.spaces import Box, Tuple, Discrete
+from gym.spaces import Box, Tuple, MultiDiscrete
 import numpy as np
 import random
 import librosa
@@ -14,177 +14,127 @@ class VSTEnv(gym.Env):
         self.config = config
         self.vst_config = vst_config
 
-        # Number of VST knobs to operate
         self.num_knobs = len(vst_config['rnd'])
-        self.num_audio_samples = int(config['sampleRate'] * config['renderLength'])
-        self.num_audio_samples = self.num_audio_samples - (self.num_audio_samples % config['fftSize']) # Keep audio samples divisible by fftSize
+        self.num_audio_samples = int(config['sampleRate'] * config['renderLength']) # Keep audio samples divisible by fftSize
+        self.num_audio_samples = self.num_audio_samples - (self.num_audio_samples % config['fftSize'])
         self.num_freq = int(1+(config['fftSize']/2.0))
         self.num_windows = int((self.num_audio_samples / config['fftSize'] - 1.0) * (config['fftSize'] / config['hopSize']) + 1.0)
-        print(self.num_freq, self.num_windows)
 
         # Mapping from action index (0, 1, ..., num_knobs) to VST parameter
-        #self.action_to_param_dict = dict(enumerate(vst_config['rnd'].keys()))
         self.action_to_param = list(vst_config['rnd'].keys())
 
-
-        ## For now, operate all knobs at once
-        #self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(self.num_knobs,))
-        #self.action_space = Tuple((
-        #    Discrete(self.num_knobs),
-        #    Box(low=-1.0, high=1.0, shape=(1,))
-        #))
-        #self.action_space = Box(low=0.0, high=self.num_knobs*2.0, shape=(1,))
-        self.action_space = Discrete(self.num_knobs*4)
-
-        # observation = state of VST knobs + rendered audio patch
-        #self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(self.num_knobs + self.num_audio_samples,))
-        #self.observation_space = spaces.Tuple((
-        #    spaces.Box(low=0.0, high=1.0, shape=(self.num_knobs,)),
-        #    spaces.Box(low=0.0, high=1.0, shape=(self.num_freq, self.num_windows)),
-        #    spaces.Box(low=0.0, high=1.0, shape=(self.num_freq, self.num_windows))
-        #))
-        #self.observation_space = spaces.Box(low=0.0, high=1.0, shape=(self.num_knobs + 2*self.num_freq*self.num_windows,))
-        self.observation_space = spaces.Box(low=0.0, high=1.0, shape=(3, self.num_freq,self.num_windows,))
+        self.action_space = MultiDiscrete([self.num_knobs, 4])
+        self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(self.num_freq, self.num_windows,))
+        #self.observation_space = spaces.Box(low=0, high=255, shape=(self.num_freq, self.num_windows, 1))
 
         # Create VST engine and generator
         self.engine = rm.RenderEngine(config['sampleRate'], config['bufferSize'], config['fftSize'])
         self.engine.load_plugin(vst_config['vstPath'])
         self.generator = rm.PatchGenerator(self.engine)
 
-        # Other self params
-        self.knob_state = {}
-        self.cur_audio = None
-        self.cur_stft = None
-        self.target_audio = None
-        self.target_stft = None
-        self.cur_metric = 0.0
-        self.old_metric = 0.0
-
-        self.cur_step = 0
-
     # Init VST engine params from 'self.vst_config'
     def init_engine(self):
+        knob_state = {}
         for param, value in self.vst_config['init'].items():
             if param in self.vst_config['rnd']:
-                self.knob_state[param] = value
+                knob_state[param] = value
             self.engine.override_plugin_parameter(param, value)
+        return knob_state
 
     # Randomize specific set of params as defined in 'self.vst_config'
-    def randomize_engine(self):
+    def randomize_knobs(self, knob_state):
+        knob_state = {}
         for param, value in self.vst_config['rnd'].items():
             rnd = random.uniform(value[0], value[1])
             rnd = rnd - rnd % 0.01
-            self.knob_state[param] = rnd
-        self.set_engine()
-        self.target_knobs = self.knob_state.copy()
-
-    # Set params (the ones defined to be randomized) to specific values specified in 'self.state'
-    def set_engine(self):
-        for param, value in self.knob_state.items():
-            self.engine.override_plugin_parameter(param, value)
+            knob_state[param] = rnd
+        return knob_state
 
     # Render an audio patch, safe to 'self.cur_audio'
-    def render_patch(self):
-        self.set_engine()
+    def render_audio(self, knob_state):
+        for param, value in knob_state.items():
+            self.engine.override_plugin_parameter(param, value)
         self.engine.render_patch(self.config['midiNote'], self.config['midiVelocity'], self.config['noteLength'], self.config['renderLength'])
-        self.cur_audio = np.array(self.engine.get_audio_frames())[:self.num_audio_samples]
+        audio = np.array(self.engine.get_audio_frames())[:self.num_audio_samples]
+        return audio
 
     # Compute stft of audio patch, safe to 'self.cur_stft'
-    def compute_stft(self):
-        self.cur_stft = np.abs(librosa.stft(y=self.cur_audio, hop_length=self.config['hopSize'], center=False))
-        self.cur_stft = (self.cur_stft - np.min(self.cur_stft)) / np.max(self.cur_stft) # Normalize absolute amplitudes to value range [0,1]
-
-    #def compute_similarity(self):
-    #    gaussian_kernel = lambda x, y, sigma: np.exp((-(np.linalg.norm(x-y)))/(2*sigma**2))
-    #    self.old_similarity = self.cur_similarity
-    #    self.cur_similarity = gaussian_kernel(target_audio, cur_audio, 5.0)
+    def compute_stft(self, audio):
+        stft = np.abs(librosa.stft(y=audio, hop_length=self.config['hopSize'], center=False))
+        stft = (stft - np.min(stft)) / np.max(stft) # Normalize absolute amplitudes to value range [0,1]
+        return stft
 
     def compute_reward(self):
-        self.old_metric = self.cur_metric
+        # similarity := 1/(1+dist)
+        # dist is scaled to range [0,4]
+        #similarity = lambda x, y: 1/(1+(4*np.linalg.norm(self.target_stft - self.cur_stft)/(4.0*self.num_freq*self.num_windows)))
+        similarity = lambda x, y: 1/(1+np.linalg.norm(self.goal_stft - self.stft))
 
-        self.frobenius_matrix = np.square(np.abs(self.target_stft - self.cur_stft))
-        self.cur_metric = float(np.sqrt(np.sum(self.frobenius_matrix)))
-
-        metric_dif = float(self.old_metric - self.cur_metric)
-
-        if metric_dif < 0.0:
-            self.reward = -1.0
-        elif metric_dif > 0.0:
-            self.reward = 1.0
+        if not hasattr(self, 'similarity'):
+            self.similarity = similarity(self.goal_stft, self.stft)
         else:
-            self.reward = -0.5
+            self.old_similarity = self.similarity
+            self.similarity = similarity(self.goal_stft, self.stft)
+            self.reward = float(self.similarity - self.old_similarity)
+            self.done = self.similarity > 0.95
 
-        if self.cur_step % 100 == 0:
-            z = set(self.knob_state) & set(self.target_knobs)
-            asd = ''
-            for i in z:
-                asd = asd + str(i) + ": (" + '{:0.2f}'.format(self.knob_state[i]) + " / " + '{:0.2f}'.format(self.target_knobs[i]) + "), "
-            print(self.cur_step, asd, self.cur_metric, self.reward)
-        #self.done = bool(np.all(self.frobenius_matrix < self.config['epsilon']**2))
-        self.done = self.cur_metric < 0.1
-        if self.done:
-            print('DONE')
-            z = set(self.knob_state) & set(self.target_knobs)
-            asd = ''
-            for i in z:
-                asd = asd + str(i) + ": (" + '{:0.2f}'.format(self.knob_state[i]) + " / " + '{:0.2f}'.format(self.target_knobs[i]) + "), "
-            print(self.cur_step, asd, self.cur_metric, self.reward)
+        #if self.cur_step % 100 == 0:
+        #z = set(self.knob_state) & set(self.target_knobs)
+        #asd = ''
+        #for i in z:
+        #    asd = asd + str(i) + ": (" + '{:0.2f}'.format(self.knob_state[i]) + " / " + '{:0.2f}'.format(self.target_knobs[i]) + "), "
+        #print(self.cur_step, asd, self.cur_similarity, self.reward)
+
+        #if self.done:
+        #    z = set(self.knob_state) & set(self.target_knobs)
+        #    asd = '(Done) - '
+        #    for i in z:
+        #        asd = asd + str(i) + ": (" + '{:0.2f}'.format(self.knob_state[i]) + " / " + '{:0.2f}'.format(self.target_knobs[i]) + "), "
+        #    print(self.cur_step, asd, self.cur_similarity, self.reward)
 
     def step(self, action):
-        #self.cur_action = action
-        #knob = int(action[0] // 2.0)
-        #amount = (action[0] % 2.0) - 1.0
-        self.cur_action = action
-        knob = action // 4
-        amount = 0.1 if action % 2 == 0 else 0.05
-        amount = -amount if action % 4 < 2 else amount
+        knob = action[0]
+        amount = 0.1 if action[1] % 2 == 0 else 0.05
+        amount = -amount if action[1] < 2 else amount
 
         # Apply knob adjustments, clip VST param to [0, 1.0]
         clip = lambda x, l, u: max(l, min(u, x))
         #self.knob_state[self.action_to_param[action[0]]] = clip(self.knob_state[self.action_to_param[action[0]]] + action[1][0], 0, 1.0)
         self.knob_state[self.action_to_param[knob]] = clip(self.knob_state[self.action_to_param[knob]] + amount, 0, 1.0)
 
-
         # Render audio, compute stft and reward
-        self.set_engine()
-        self.render_patch()
-        self.render_patch()
-        self.compute_stft()
+        self.audio= self.render_audio(self.knob_state)
+        self.stft = self.compute_stft(self.audio)
         self.compute_reward()
 
-        #observation = np.concatenate((np.array(list(self.knob_state.values())).flatten(), self.target_stft.flatten(), self.cur_stft.flatten()))
-        observation = np.stack((self.target_stft, self.cur_stft, np.zeros((self.num_freq, self.num_windows))))
+        #observation = np.expand_dims((self.stft - self.goal_stft + 1.0) * 128.0, axis=2)
+        observation = self.stft - self.goal_stft
         info = {}
 
-        if self.cur_step % 100 == 0:
-            write(str(self.cur_step) + '.wav', self.config['sampleRate'], self.cur_audio)
-        self.cur_step += 1
-
+        #if self.cur_step % 100 == 0:
+        #    write(str(self.cur_step) + '.wav', self.config['sampleRate'], self.cur_audio)
         return observation, self.reward, self.done, info
 
     def reset(self):
-        # Create random target
-        self.init_engine() # Init engine params
-        self.randomize_engine() # Randomize engine params
-        self.render_patch() # Render audio
-        self.render_patch()
-        self.target_audio = np.copy(self.cur_audio)
+        self.iter = 0
 
-        self.compute_stft() # Compute stft
-        self.target_stft = np.copy(self.cur_stft)
-        write('target.wav', self.config['sampleRate'], self.target_audio)
+        # Create goal
+        self.goal_knob_state = self.init_engine() # Init engine params
+        self.randomize_knobs(self.goal_knob_state) # Randomize engine params
+        self.goal_audio = self.render_audio(self.goal_knob_state) # Render audio
+        self.goal_stft = self.compute_stft(self.goal_audio) # Compute stft
+        #write('target.wav', self.config['sampleRate'], self.target_audio)
+
         # Reinitialize VST engine to start state
-        self.init_engine()
-        self.render_patch()
-        self.compute_stft()
+        self.knob_state = self.init_engine()
+        self.audio = self.render_audio(self.knob_state)
+        self.stft = self.compute_stft(self.audio)
 
         # Compute similarity between target and start
         self.compute_reward()
 
-        self.cur_step = 0
-
-        #observation = np.concatenate((np.array(list(self.knob_state.values())).flatten(), self.target_stft.flatten(), self.cur_stft.flatten()))
-        observation = np.stack((self.target_stft, self.cur_stft, np.zeros((self.num_freq, self.num_windows))))
+        #observation = np.expand_dims((self.cur_stft - self.target_stft + 1.0) * 128.0, axis=2)
+        observation = self.stft - self.goal_stft
         return observation
 
     def render(self, mode='console'):
--- main.py
+++ main.py
@@ -6,8 +6,30 @@ from stable_baselines.common.env_checker import check_env
 from stable_baselines.common.policies import MlpPolicy, CnnPolicy
 #from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines.common import make_vec_env
-from stable_baselines import A2C, DQN
+from stable_baselines import A2C, DQN, PPO2
+from stable_baselines.common.callbacks import BaseCallback
+import tensorflow as tf
 
+
+#from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines.common import set_global_seeds
+from stable_baselines.common.evaluation import evaluate_policy
+from stable_baselines.common.cmd_util import make_vec_env
+
+class TensorboardCallback(BaseCallback):
+    def __init__(self, env, verbose=0):
+        self.env = env
+        self.first = True
+        super(TensorboardCallback, self).__init__(verbose)
+
+    def _on_step(self):
+        if self.first:
+            goal_stft = tf.expand_dims(tf.convert_to_tensor(self.env.goal_stft), 0)
+            summary = tf.compat.v1.summary.image('goal_stft', goal_stft)
+            #self.locals['writer'].add_summary(summary, self.num_timesteps)
+            #print("wow")
+            self.first = False
+        return True
 
 def load_configs():
     with open("./config.yaml", 'r') as stream:
@@ -29,17 +51,39 @@ def symlink_librenderman(config):
         os.symlink(config['libPath'], os.path.join('.', 'librenderman.so'))
     import librenderman as rm
 
+def make_env(config, config_vst, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environment you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = VSTEnv(config, config_vst)
+        env.seed(seed + rank)
+        return env
+
+    set_global_seeds(seed)
+    return _init
+
 
 # Load configs, import librenderman
 config, config_vst = load_configs()
 symlink_librenderman(config)
 
+"""
+num_cpu = 4
+env = SubprocVecEnv([make_env(config, config_vst, i) for i in range(num_cpu)])
+model = PPO2(CnnPolicy, env)
+model.learn(total_timesteps=50000)
+model.save('ppo2_test')
+"""
 # Create name
 env = VSTEnv(config, config_vst)
 #model = A2C(MlpPolicy, env, verbose=1)
-model = A2C(CnnPolicy, env, verbose=1)
-model.learn(total_timesteps=50000)
-#model = DQN(MlpPolicy, env, verbose=1)
-#model.learn(total_timesteps=50000)
-model.save('test1')
+model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='tensorboard/')
+model.learn(total_timesteps=50000, callback=TensorboardCallback(env))
+model.save('ppo2_test')
 #check_env(env, warn=True#)
